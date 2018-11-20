bcec7833831c7e5b8a6395faf75e461677512473
balakrishnacs <bala07205@gmail.com>
2018-11-20 22:23:03 +0530

    First commit

---
+++ src/com/bala/EmployeeTest.java
@@ -0,0 +1,28 @@
+package com.bala;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*Employee e1=new Employee(1, "bala", "Krishna");
+		Employee e2=new Employee(2,"Aion","Naidu");
+		Employee e3=new Employee(3,"Hema","Reddy");
+		jre1.8.0_181
+		List<Employee> list =new ArrayList<>();
+		list.add(e1);
+		list.add(e2);
+		list.add(e3);*/
+		String s1=new String("durga");
+		String s2="durga";
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));
+	//	Collections.sort(list);
+
+	}
+
+}
---
+++ src/com/bala/LamdaTest.java
@@ -0,0 +1,27 @@
+package com.bala;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@FunctionalInterface
+interface MyFunctionalInterface{
+	
+	public String sayHello();
+}
+public class LamdaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List list=new ArrayList();
+		list.add(10);
+		list.add(12);
+		list.add(15);
+		System.out.println("list :"+list);
+		
+		
+		
+		
+	}
+}
+	
